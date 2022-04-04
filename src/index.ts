cbd5244c2c24789b50036d21c18553f25830c2cd
Karishma Ghiya <kaghiya@microsoft.com>
2022-04-04 16:01:49 -0700

    Code working with login, logout and sessions

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,43 @@ import * as express from "express";
 import { Server } from "http";
 import { readFileSync } from "fs";
 import * as bodyParser from "body-parser";
-import { verifyLogin } from "./loginVerification";
 import * as session from "express-session";
+import * as connectEnsureLogin from "connect-ensure-login";
 import * as passport from "passport";
-import * as connect-ensure-login from "connect-ensure-login";
+import {Strategy} from "passport-local";
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false })); // The object req.body will contain key-value pairs, where the value can be a string or array (when extended is false), or any type (when extended is true)
+
+app.use(session({
+  secret: 'r8q,+&1LM3)CD*zAGpx1xm{NeQhc;#',
+  resave: true,
+  saveUninitialized: true}));
+// Configure More Middleware
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new Strategy(
+  function(username, password, done) {
+    if(username === "karishma"){
+      if(password === "ghiya"){
+        console.log("success login");
+        return done(null,{username:"karishma"});
+      }
+      else{
+        console.log("incorrect pwd");
+      return done(new Error("Incorrect password!"), false);
+      }
+    }
+    else{
+      console.log("incorrect username");
+      return done(new Error("Username doesn't exist!"), false);
+    }
+
+  }
+));
 //logs every request
 app.use(
   (req: express.Request, res: express.Response, next: express.NextFunction) => {
@@ -17,17 +46,15 @@ app.use(
     next();
   }
 );
-app.use(session({
-  secret: 'r8q,+&1LM3)CD*zAGpx1xm{NeQhc;#',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 60 * 60 * 1000 } // 1 hour
-}));
 
-// Configure More Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(passport.initialize());
-app.use(passport.session());
+// To use with sessions
+passport.serializeUser(function(user){
+  console.log(user);
+  console.log(JSON.stringify(user,null,2));
+ return JSON.stringify(user,null,2);
+});
+passport.deserializeUser(function(user){ return JSON.parse(user)});
+
 /**
  * Endpoint that loads the index.js
  */
@@ -50,16 +77,50 @@ app.get("/index", async (req: express.Request, res: express.Response) => {
  * Home URI
  */
  app.get("/login", async (req: express.Request, res: express.Response) => {
+   console.log("get login");
   const indexContent = readFileSync("./login.html", { encoding: "utf8" });
   res.send(indexContent);
 });
 
-app.post('/login', (req: express.Request, res: express.Response) => {
-  // Insert Login Code Here
-  let username = req.body.username;
-  let password = req.body.password;
-  res.send(verifyLogin(username,password));
+app.post('/login', function(req, res, next) {
+ passport.authenticate('local', function(err, user, info, status) {
+  if (err) { return next(err) }
+    if (!user) { 
+      console.log(`This ${user} seems invalid!`);
+      return res.redirect('/login') }
+    res.redirect('/dashboard');
+  })(req, res, next);
 });
+
+// app.post('/login', passport.authenticate('local',{failureRedirect: '/login', successReturnToOrRedirect: '/dashboard'}));
+
+app.get('/dashboard', (req, res) => {
+  console.log("get dashboard");
+  res.send(`Hello! Your authentication was successful. Your session ID is ${req.sessionID} 
+   and your session expires in ${req.session.cookie.maxAge} 
+   milliseconds.<br><br>
+   <a href="/logout">Log Out</a><br><br>
+   <a href="/secret">Members Only</a>`);
+});
+
+app.get('/secret', connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+  res.sendFile(__dirname + './userSession.html');
+});
+
+app.get('/logout', function(req: express.Request, res: express.Response) {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.status(400).send('Unable to log out')
+      } else {
+        res.send('Logout successful! Go back to login here - <a href="/login">Please Login Here!</a>')
+      }
+    });
+  } else {
+    res.end()
+  }
+});
+
 
 let server: Server | undefined = undefined;
 server = app.listen(8080, () => {
